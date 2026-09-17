371f9d74f206b1e8d9c4f59754a21986468c3d37
Sen <senahchoi@gmail.com>
2026-09-16 20:31:57 -0700

    Added initial basic codes

---
+++ Drawing/sketch.js
@@ -0,0 +1,12 @@
+console.log("This is my amazing drawing");
+
+function setup() {
+  createCanvas(900, 900);
+}
+
+function draw() {
+  background(10);
+  fill(100, 100, 100);
+  circle(100, 100, 100);
+  circle(100, 100, 100);
+}
